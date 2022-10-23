314c219efa90d01b19d4699ef96d38cee9436209
DeveloperAlex0 <100363048+DeveloperAlex0@users.noreply.github.com>
2022-10-23 21:56:11 +0300

    Add files via upload

---
+++ KillRoblox/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KillRoblox
+{
+    internal class Program
+    {
+        public static void Print(string text, int speed)
+        {
+            foreach (char value in text)
+            {
+                Console.Write(value);
+                Thread.Sleep(speed);
+            }
+            Console.WriteLine();
+        }
+
+        public static void change(string text)
+        {
+            Console.Title = text;
+        }
+
+        public static void killroblox()
+        {
+            Process[] processesByName = Process.GetProcessesByName("RobloxPlayerBeta");
+            int num = 0;
+            for (int i = 0; i < processesByName.Length; i++)
+            {
+                processesByName[i].Kill();
+                num++;
+            }
+            Program.Print(string.Format("Terminated {0} Process", num), 25);
+        }
+
+        static void Main(string[] args)
+        {
+            Program.change("Kill Roblox Tool");
+            Program.killroblox();
+            //Program.Print("We are done! Closing this in 3 seconds...", 25);
+            //Thread.Sleep(3000);
+            Program.Print("Press any key to exit...", 25);
+            Console.ReadKey();
+        }
+
+
+    }
+}
---
+++ KillRoblox/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("KillRoblox")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KillRoblox")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("9a48d060-662d-4e62-ae71-b9ad79793b87")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
